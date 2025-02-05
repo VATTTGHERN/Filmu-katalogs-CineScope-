4e6144270a8d7c211af01f70c5a0f6adc694f3ab
JumpyW <maks8848gold@gmail.com>
2025-02-05 18:45:52 +0200

    Update config.py

--- backend/cinescope_backend/app/config.py
+++ backend/cinescope_backend/app/config.py
@@ -4,3 +4,5 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:CineScope2025!@localhost/cinescope_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = "a94c1fcb6a97b1d0cbe8725af7e890dd83a2d5a830f1c2b9b7c6de59f7c3eabb"
