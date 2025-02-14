b193bb4686b177272d20d8b0752e27915a405127
JumpyW <maks8848gold@gmail.com>
2025-02-14 16:32:26 +0200

    Update config.py

--- backend/cinescope_backend/app/config.py
+++ backend/cinescope_backend/app/config.py
@@ -1,8 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    SECRET_KEY = "a94c1fcb6a97b1d0cbe8725af7e890dd83a2d5a830f1c2b9b7c6de59f7c3eabb"
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:CineScope2025!@localhost/cinescope_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SECRET_KEY = "a94c1fcb6a97b1d0cbe8725af7e890dd83a2d5a830f1c2b9b7c6de59f7c3eabb"
+    # 🔐 Настройки JWT
+    JWT_SECRET_KEY = "super_secret_jwt_key"  # Можешь заменить на свой
+    JWT_TOKEN_LOCATION = ["headers"]  # Токен передается в заголовке запроса
+    JWT_ACCESS_TOKEN_EXPIRES = 3600  # Время жизни токена (1 час)
