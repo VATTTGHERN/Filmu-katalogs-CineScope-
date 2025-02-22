b87e9bbf5c4d7d01525000fce36d48f764edc6e5
JumpyW <maks8848gold@gmail.com>
2025-02-22 12:18:33 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MovieCatalog from "./pages/MovieCatalog";
+import MovieCatalog from "./pages/MovieCatalog";  // <-- добавляем `pages/`
+import MovieDetails from "./pages/MovieDetails";  // <-- добавляем `pages/`
+import LatvianMovies from "./pages/LatvianMovies";
+import Login from "./pages/Login";  // <-- добавляем `pages/`
+import Register from "./pages/Register";  // <-- добавляем `pages/`
+import "./styles/global.css"; // Подключаем общий CSS
 
-function App() {
+
+const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<h1>Добро пожаловать в CineScope</h1>} />
                 <Route path="/catalog" element={<MovieCatalog />} />
+                <Route path="/movie/:id" element={<MovieDetails />} />
+                <Route path="/latvian-movies" element={<LatvianMovies />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="*" element={<MovieCatalog />} /> {/* Перенаправление на /catalog по умолчанию */}
             </Routes>
         </Router>
     );
-}
+};
 
 export default App;
