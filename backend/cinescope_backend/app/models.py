5f892de302fb1403fad1e03b3693584f575114d7
JumpyW <maks8848gold@gmail.com>
2025-02-25 18:12:27 +0200

    Update models.py

--- backend/cinescope_backend/app/models.py
+++ backend/cinescope_backend/app/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(20), nullable=False, default="user")  # <=== Дефолтное значение 'user'
+    is_blocked = db.Column(db.Boolean, default=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
