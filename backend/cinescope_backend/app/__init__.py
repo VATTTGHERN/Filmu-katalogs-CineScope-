cf5b41f674e0ba934bc0abede19db50e402c1d88
JumpyW <maks8848gold@gmail.com>
2025-06-02 17:29:25 +0300

    Update __init__.py

--- backend/cinescope_backend/app/__init__.py
+++ backend/cinescope_backend/app/__init__.py
@@ -1,35 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager  # ✅ Добавляем JWT
+from flask_jwt_extended import JWTManager 
 
-# Создаем экземпляры SQLAlchemy и JWT
 db = SQLAlchemy()
-jwt = JWTManager()  # ✅ Создаем экземпляр JWT
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
 
-    # Конфигурации приложения
     app.config.from_object('app.config.Config')
 
-    # ✅ Разрешаем CORS
     CORS(app, supports_credentials=True, origins="http://localhost:5173")
 
-    # Инициализация расширений
     db.init_app(app)
-    jwt.init_app(app)  # ✅ Инициализируем JWT в приложении
+    jwt.init_app(app) 
 
-    # Подключение моделей
     with app.app_context():
-        from app.models import User, Movie  # Явный импорт моделей
-        db.create_all()  # Создаем таблицы в базе данных, если их еще нет
+        from app.models import User, Movie 
+        db.create_all() 
 
-        # Регистрация маршрутов
         from app.routes import bp
         app.register_blueprint(bp)
 
-        from app.auth import auth_bp  # ✅ Подключаем маршруты аутентификации
+        from app.auth import auth_bp 
         app.register_blueprint(auth_bp, url_prefix="/auth")
 
     return app
