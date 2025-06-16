e3495237d460727492dd98e568b7fbebc80d8822
JumpyW <maks8848gold@gmail.com>
2025-06-16 14:19:06 +0300

    Update FavoriteMovies.jsx

--- frontend/src/pages/FavoriteMovies.jsx
+++ frontend/src/pages/FavoriteMovies.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/MovieCatalog.css"; //Izmantojam tos pašus stilus kā katalogam.
 
+// Favoritu saraksts
 const FavoriteMovies = () => {
     const [favorites, setFavorites] = useState([]); // Lietotāja favorītfilmas
     const navigate = useNavigate();
