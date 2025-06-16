2cadc0116681a1230199d7d7cce9c457401095bf
JumpyW <maks8848gold@gmail.com>
2025-06-16 13:13:43 +0300

    Update config.py

--- backend/cinescope_backend/app/config.py
+++ backend/cinescope_backend/app/config.py
@@ -5,8 +5,8 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:CineScope2025!@localhost/cinescope_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = "super_secret_jwt_key"  # Можешь заменить на свой
+    JWT_SECRET_KEY = "super_secret_jwt_key"
     JWT_TOKEN_LOCATION = ["headers"]
-    JWT_ACCESS_TOKEN_EXPIRES = 3600  # Время жизни токена (1 час)
+    JWT_ACCESS_TOKEN_EXPIRES = 3600 
 
     CORS_HEADERS = "Content-Type"
